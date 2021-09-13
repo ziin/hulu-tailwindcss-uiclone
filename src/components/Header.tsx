f12003dcf6b48a212f484970c7e88db2400ec596
Alisson L. Schwanz <alissonludtkeschwanz@gmail.com>
2021-09-13 17:17:21 -0400

    feat: turn header icons into links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,8 +14,12 @@ function Header() {
   return (
     <header className="flex flex-col sm:flex-row justify-between items-center m-5 h-auto">
       <div className="flex flex-grow justify-evenly max-w-2xl">
-        <HeaderItem title="HOME" Icon={HomeIcon} />
-        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="HOME" Icon={HomeIcon} href="/" />
+        <HeaderItem
+          title="TRENDING"
+          Icon={LightningBoltIcon}
+          href="?genre=trending"
+        />
         <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
         <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
         <HeaderItem title="SEARCH" Icon={SearchIcon} />
--- src/components/HeaderItem.tsx
+++ src/components/HeaderItem.tsx
@@ -1,14 +1,19 @@
-function HeaderItem({ title, Icon }: Props) {
+import Link from 'next/link'
+
+function HeaderItem({ title, Icon, href = '/' }: Props) {
   return (
-    <div className="group flex flex-col items-center cursor-pointer w-12 sm:w-20 hover:text-white">
-      <Icon className="h-8 mb-1 group-hover:animate-bounce" />
-      <p className="tracking-widest invisible group-hover:visible">{title}</p>
-    </div>
+    <Link href={href} passHref>
+      <div className="group flex flex-col items-center cursor-pointer w-12 sm:w-20 hover:text-white">
+        <Icon className="h-8 mb-1 group-hover:animate-bounce" />
+        <p className="tracking-widest invisible group-hover:visible">{title}</p>
+      </div>
+    </Link>
   )
 }
 
 type Props = {
   title: string
+  href?: string
   Icon: (props: React.ComponentProps<'svg'>) => JSX.Element
 }
 
